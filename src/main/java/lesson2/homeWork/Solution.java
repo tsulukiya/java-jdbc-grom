546d7c86ee5263455d19fb419def6624827daab0
macbook <tsulukiya86@gmail.com>
2019-04-03 18:24:41 +0300

    first lesson JDBC - complete...

--- src/main/java/lesson2/homeWork/Solution.java
+++ src/main/java/lesson2/homeWork/Solution.java
@@ -1,15 +1,14 @@
 package lesson2.homeWork;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Solution {
     private static final String DB_URL = "jdbc:oracle:thin:@gromcode-lessons.clg2f8ljlc7q.us-east-2.rds.amazonaws.com:1521:ORCL";
-
     private static final String USER = "main";
     private static final String PASS = "TsulukiyA_123";
+    
 
     private static void saveProduct() {
         try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASS); Statement statement = connection.createStatement()) {
@@ -42,5 +41,64 @@ public class Solution {
             e.printStackTrace();
 
         }
+    }
+
+    private static Set<Product> getAllProducts() {
+        Set<Product> productSet = new HashSet<>();
+        try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASS); Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM PRODUCT");
+            while (resultSet.next()) {
+                long id = resultSet.getLong(1);
+                String name = resultSet.getString(2);
+                String description = resultSet.getString(3);
+                int price = resultSet.getInt(4);
+                productSet.add(new Product(id, name, description, price));
+
+            }
+        } catch (SQLException e) {
+            System.err.println("Something went wrong");
+            e.printStackTrace();
+
+        }
+        return productSet;
+    }
+
+    private static Set<Product> getProductsByPrice() {
+        Set<Product> productSet = new HashSet<>();
+        try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASS); Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM PRODUCT WHERE PRICE <= 100 ");
+            while (resultSet.next()) {
+                long id = resultSet.getLong(1);
+                String name = resultSet.getString(2);
+                String description = resultSet.getString(3);
+                int price = resultSet.getInt(4);
+                productSet.add(new Product(id, name, description, price));
+
+            }
+        } catch (SQLException e) {
+            System.err.println("Something went wrong");
+            e.printStackTrace();
+
+        }
+        return productSet;
+    }
+
+    private static Set<Product> getProductsByDescription() {
+        Set<Product> productSet = new HashSet<>();
+        try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASS); Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM PRODUCT");
+            while (resultSet.next() && resultSet.getString(3).toCharArray().length > 50) {
+                long id = resultSet.getLong(1);
+                String name = resultSet.getString(2);
+                String description = resultSet.getString(3);
+                int price = resultSet.getInt(4);
+                productSet.add(new Product(id, name, description, price));
+            }
+        } catch (SQLException e) {
+            System.err.println("Something went wrong");
+            e.printStackTrace();
+
+        }
+        return productSet;
     }
 }
