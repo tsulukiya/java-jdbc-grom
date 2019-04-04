f8653f3a1868c4ffa0a51144236691ea3effdbb9
macbook <tsulukiya86@gmail.com>
2019-04-04 09:20:49 +0300

    second lesson JDBC - er...

--- src/main/java/lesson2/homeWork/Solution.java
+++ src/main/java/lesson2/homeWork/Solution.java
@@ -8,11 +8,19 @@ public class Solution {
     private static final String DB_URL = "jdbc:oracle:thin:@gromcode-lessons.clg2f8ljlc7q.us-east-2.rds.amazonaws.com:1521:ORCL";
     private static final String USER = "main";
     private static final String PASS = "TsulukiyA_123";
-    
+
+    public static void main(String[] args) {
+        //saveProduct();
+        //increasePrice();
+
+        changeDescription();
+
+    }
+
 
     private static void saveProduct() {
         try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASS); Statement statement = connection.createStatement()) {
-            int response = statement.executeUpdate("INSERT INTO PRODUCT VALUES (999, 'toy111', 'for children', 60)");
+            int response = statement.executeUpdate("INSERT INTO PRODUCT VALUES (999, 'toy', 'for children', 60)");
             System.out.println(response);
         } catch (SQLException e) {
             System.err.println("Something went wrong");
@@ -87,12 +95,14 @@ public class Solution {
         Set<Product> productSet = new HashSet<>();
         try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASS); Statement statement = connection.createStatement()) {
             ResultSet resultSet = statement.executeQuery("SELECT * FROM PRODUCT");
-            while (resultSet.next() && resultSet.getString(3).toCharArray().length > 50) {
-                long id = resultSet.getLong(1);
-                String name = resultSet.getString(2);
-                String description = resultSet.getString(3);
-                int price = resultSet.getInt(4);
-                productSet.add(new Product(id, name, description, price));
+            while (resultSet.next()) {
+                if (resultSet.getString(3).toCharArray().length > 50) {
+                    long id = resultSet.getLong(1);
+                    String name = resultSet.getString(2);
+                    String description = resultSet.getString(3);
+                    int price = resultSet.getInt(4);
+                    productSet.add(new Product(id, name, description, price));
+                }
             }
         } catch (SQLException e) {
             System.err.println("Something went wrong");
@@ -100,5 +110,43 @@ public class Solution {
 
         }
         return productSet;
+    }
+
+    private static void increasePrice() {
+        try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASS); Statement statement = connection.createStatement()) {
+            int response = statement.executeUpdate("UPDATE PRODUCT SET PRICE = PRICE + 100 WHERE PRICE < 970");
+            System.out.println(response);
+        } catch (SQLException e) {
+            System.err.println("Something went wrong");
+            e.printStackTrace();
+
+        }
+
+    }
+
+    private static void changeDescription() {
+        try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASS); Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM PRODUCT");
+            while (resultSet.next()) {
+                if (resultSet.getString(3).toCharArray().length > 100) {
+                    String res = replaceDescription(resultSet.getString(3));
+                    String query = "UPDATE PRODUCT SET DESCRIPTION =" + res;
+                    int response = statement.executeUpdate(query);
+                    System.out.println(response);
+                }
+
+            }
+        } catch (SQLException e) {
+            System.err.println("Something went wrong");
+            e.printStackTrace();
+
+        }
+    }
+
+    private static String replaceDescription(String text) {
+        String[] mas = text.split("\\.");
+        String delete = mas[mas.length - 1];
+        String result = text.replace(delete, "");
+        return result;
     }
 }
