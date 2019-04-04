6eaa5ace17c439071a892cc2914dff4342366c70
macbook <tsulukiya86@gmail.com>
2019-04-04 23:34:36 +0300

    third lesson - part1

--- src/main/java/lesson3/homeWork/part1/Demo.java
+++ src/main/java/lesson3/homeWork/part1/Demo.java
@@ -1,18 +1,10 @@
 package lesson3.homeWork.part1;
 
-import lesson3.classWork.ProductDAO;
-
 public class Demo {
     public static void main(String[] args) {
-        ProductDAO productDAO = new ProductDAO();
         Solution solution = new Solution();
-        Product product = new Product(102, "test111", "test description", 99);
-        //productDAO.save(product);
-        //System.out.println(productDAO.getProduct());
-        //productDAO.delete(997);
-        //productDAO.update(product);
-        //System.out.println(solution.findProductsByPrice(40, 100));
-        //System.out.println(solution.findProductByName("111"));
+        System.out.println(solution.findProductsByPrice(40, 100));
+        System.out.println(solution.findProductByName("111"));
         System.out.println(solution.findProductByEmptyDescription());
 
 
