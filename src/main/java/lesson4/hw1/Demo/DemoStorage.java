4b4e82a4b92f96a595220de90266084f70510958
macbook <tsulukiya86@gmail.com>
2019-05-06 23:50:46 +0300

    4th lesson - HW

--- src/main/java/lesson4/hw1/Demo/DemoStorage.java
+++ src/main/java/lesson4/hw1/Demo/DemoStorage.java
@@ -5,9 +5,17 @@ import lesson4.hw1.model.Storage;
 
 public class DemoStorage {
     public static void main(String[] args) {
-        Storage storage = new Storage(1004, "jpg,png", "Ukraine", 2202);
+        Storage storage = new Storage(1005, "jpg,png", "Ukraine", 22002);
+        Storage storage1 = new Storage(1002, "jpg,png", "Ukraine", 22002);
         StorageController storageController = new StorageController();
-        storageController.save(storage);
+
+        //storageController.save(storage);
+
+        //storageController.delete(1004);
+
+        //storageController.update(storage1);
+
+        System.out.println(storageController.findById(1002));
     }
 
 }
--- src/main/java/lesson4/hw1/repository/ShareRepository.java
+++ src/main/java/lesson4/hw1/repository/ShareRepository.java
@@ -1,44 +1,18 @@
 package lesson4.hw1.repository;
 
-import lesson4.cw.Product;
-import lesson4.hw1.model.File;
-import lesson4.hw1.model.Storage;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public abstract class ShareRepository<T> {
+public abstract class ShareRepository {
     private static final String DB_URL = "jdbc:oracle:thin:@gromcode-lessons.clg2f8ljlc7q.us-east-2.rds.amazonaws.com:1521:ORCL";
     private static final String USER = "main";
     private static final String PASS = "TsulukiyA_123";
 
 
-    public T insertObjectToDb(T t) {
-
-        try (Connection connection = getConnection(); PreparedStatement preparedStatement =
-                connection.prepareStatement(sqlQueryInsertToDb())) {
-
-            connection.setAutoCommit(true);
-
-            initStatement(preparedStatement, t);
-
-        } catch (SQLException e) {
-
-            e.printStackTrace();
-        }
-        return t;
-    }
-
-    private String sqlQueryInsertToDb() {
-        return "INSERT INTO STORAGE VALUES (?, ?, ?, ?)";
-    }
-
-    abstract void initStatement(PreparedStatement preparedStatement, T t);
 
 
-    private Connection getConnection() throws SQLException {
+    public Connection getConnection() throws SQLException {
         return DriverManager.getConnection(DB_URL, USER, PASS);
     }
 
